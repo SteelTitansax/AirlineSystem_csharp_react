d8890e46770a0c8c0bfbed2bc68e747555eb547e
SteelTitansax <manuel.portero.leiva@gmail.com>
2024-06-30 20:30:11 +0200

    Final commit (layout need to be changed)

--- AirlineSystem.Api/Program.cs
+++ AirlineSystem.Api/Program.cs
@@ -1,9 +1,26 @@
 using AirlineSystem.Api.Configurations;
+using Microsoft.Net.Http.Headers;
 using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:3000")
+            .WithHeaders(new string[]
+            {
+                HeaderNames.ContentType 
+            }
+            )
+            .AllowAnyMethod();
+
+        });
+});
 
 builder.Services.AddControllers().AddJsonOptions(options =>
 {
@@ -18,6 +35,7 @@ builder.Services.AddDependencyInjectionConfiguration();
 
 var app = builder.Build();
 
+app.UseCors("AllowReactApp");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
